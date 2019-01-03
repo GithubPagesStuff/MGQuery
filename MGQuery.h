6d29177031854b16e8cf03af92713abe8d614a02
GithubPagesStuff <33375267+GithubPagesStuff@users.noreply.github.com>
2019-01-03 18:56:46 -0500

    whats a header? i dont know

---
+++ MGQuery.h
@@ -0,0 +1,12 @@
+//
+//  MGQuery.h
+//
+//  Created by purplesn0w on 1/3/19.
+//  Copyright © 2019 xeriviOS Team. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface MGQuery : NSObject
+const char *MGQueryAnswer (char *Question);
+@end
